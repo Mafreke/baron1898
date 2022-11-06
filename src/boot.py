d7a4311b35360f77d896c0caa6aa41c17e088720
Mafreke <fre.vanbuynder@gmail.com>
2022-11-06 11:10:48 +0100

    boot updated

--- src/boot.py
+++ src/boot.py
@@ -2,20 +2,26 @@ import senko
 import machine
 import network
 
+GITHUBURL = "https://github.com/Mafreke/baron1898/blob/master/src/"
+
 OTA = senko.Senko(
-    user="Fre",
-    repo="baronsim",
-    files=["boot.py", "main.py", "/lib"]
+  user="Mafreke", # Required
+  repo="baron1898", # Required
+  branch="master", # Optional: Defaults to "master"
+  working_dir="src", # Optional: Defaults to "app"
+  files = ["boot.py", "main.py"]
 )
 
 wlan = network.WLAN(network.STA_IF) # create station interface
 wlan.active(True)       # activate the interface
 wlan.scan()             # scan for access points
-wlan.isconnected()      # check if the station is connected to an AP
-wlan.connect('WiFi-2.4-E1B3', 'Amortje2016') # connect to an AP
+while not wlan.isconnected():      # check if the station is connected to an AP
+    wlan.connect('WiFi-2.4-E1B3', 'Amortje2016') # connect to an AP
 wlan.config('mac')      # get the interface's MAC address
 print(wlan.ifconfig())
 
 if OTA.update():
     print("updated to the latest version! Rebooting...")
     machine.reset()
+else:
+    print("Version up to date!")
